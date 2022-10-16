9acf23ee1c5ac1640db802bd1ac17086a55be584
songjp <songjp@gmail.com>
2022-10-16 22:55:01 +0800

    fix: 更新 centos 8.0 下安装踩坑记录

--- src/message.js
+++ src/message.js
@@ -13,15 +13,16 @@ module.exports = async function (bot, message) {
   // 只处理文字信息
   if (isText) {
     if (room) {
+      // 如果是群消息
+      const topic = await room.topic()
       if (mentions?.length) {
         // message.mentionList() 数组前面是被艾特的人，数组最后一个元素是「发消息的人，等于 fromUser」
+        console.log(`群名: ${topic} 发消息人: ${fromUser} 内容: ${content}`)
         await room.say(`
 收到消息: ${content}。
     
 ${fromUser} 不要艾特我，我什么都不会。`)
       } else {
-        // 如果是群消息 目前只处理文字消息
-        const topic = await room.topic()
         console.log(`群名: ${topic} 发消息人: ${fromUser} 内容: ${content}`)
       }
     } else {
